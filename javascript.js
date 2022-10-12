0b5125891fbaefbc7645751d8453cb75bf2df346
Rich <richchristine.rc@gmail.com>
2022-10-12 11:53:58 -0700

    -created javascript -added conditionals -created variables

---
+++ javascript.js
@@ -0,0 +1,54 @@
+let choices = ["rock","paper","scissors"];
+function getComputerChoice(){ 
+    let comChoice = Math.floor(Math.random()*choices.length) +1 ;
+if (comChoice == 3){
+        return "scissors"   
+}else if(comChoice== 2){
+    return "paper"
+}else{comChoice == 1
+    return "rock"
+};/*getComputerChoice options*/
+}
+function playRound(playerSelection,compChoice){
+  playerSelection = prompt("Rock, paper, or Scissors!").toLowerCase();
+if (playerSelection == "rock" && compChoice == "paper"){
+alert("You lose! Paper covers Rock"),
+computerScore++,
+console.log("The score is",playerScore, "-", computerScore);
+} else if (playerSelection =="rock" && compChoice == "scissors"){
+alert("You win! Rock smashes scissors"),
+playerScore++,
+console.log("The score is", playerScore, "-", computerScore);
+}else if(playerSelection == "paper" && compChoice == "scissors"){
+alert("You lose! scissors cuts paper"),
+computerScore++,
+console.log("The score is",playerScore, "-", computerScore);
+}else if(playerSelection=="paper" && compChoice=="rock"){
+alert("You win! Paper covers Rock"),
+playerScore++,
+console.log("The score is", playerScore - computerScore);
+}else if(playerSelection== "scissors" && compChoice=="rock"){
+alert("You lose! Rock smashses Scissors"),
+computerScore++,
+console.log("The score is", playerScore, "-", computerScore);
+}else if(playerSelection=="scissors"&&compChoice == "paper"){
+alert("You win! Scissors cuts Paper"),
+playerScore++,
+console.log("The score is",playerScore, "-", computerScore); 
+}
+else{
+    alert("Please input valid answer.")
+}
+}
+
+const compChoice = getComputerChoice();
+
+function game() {
+    for(let i=0;i<5;i++){
+ if( playRound()){
+    return i++;
+ } 
+    }
+}
+let playerScore = 0;
+let computerScore = 0;
